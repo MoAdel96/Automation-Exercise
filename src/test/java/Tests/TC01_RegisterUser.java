d0c597d5b53ef4c838536dd32fac6c7450a7a980
Your Name <your.email@example.com>
2024-11-13 16:55:14 +0200

    allure report

--- src/test/java/Tests/TC01_RegisterUser.java
+++ src/test/java/Tests/TC01_RegisterUser.java
@@ -180,10 +180,6 @@ public class TC01_RegisterUser {
     @Epic("Web interface")
     @Feature("Register")
     @Story("Authentication")
-
-
-
-
     public void RegisterUser(){
        //TODO: Entering name and Email
         new P01_HomePage(getDriver())
